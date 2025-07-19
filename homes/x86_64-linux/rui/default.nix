a32448938f13ecaaa4202f847530718e281949f9
RuiPMaia <ruipmaia29@gmail.com>
2025-07-19 12:31:16 +0100

    Added gh and maim

--- homes/x86_64-linux/rui/default.nix
+++ homes/x86_64-linux/rui/default.nix
@@ -28,6 +28,8 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    gh
+    maim
     stremio
     htop
     teams-for-linux
